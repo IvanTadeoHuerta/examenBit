e50b304a699f67b6707829d120f05cf8f0882453
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-11-28 13:59:26 -0600

    Se creo la peticion http get

--- src/app/services/examen.service.ts
+++ src/app/services/examen.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class ExamenService {
 
   constructor(private http:Http) { }
+
+  getClientes(){
+    this.http.get('')
+          .toPromise()
+          .then(response => response.json)
+          .catch(this.ocurrioUnError)
+  }
+
+
+  private ocurrioUnError(error:any){
+    console.log("Ocurrio un error", error)
+    return Promise.reject(error.message || error)
+  }
+
+
+
 
 }
