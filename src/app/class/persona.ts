843c747f56503dc6d0151fac85c7ce7865f2b94f
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-11-28 13:32:22 -0600

    Inicio de examen

--- src/app/class/persona.ts
+++
@@ -1,9 +0,0 @@
-export class Persona {
-    private nombre: String = '';
-    private edad: number = 0;
-    private NSS: String;
-    private sexo: String = 'H'; 
-    private peso: number = 0; 
-    private altura: number = 0;
-
-}
